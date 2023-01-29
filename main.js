bfb9aae351a53eae43d24cac6360705805b250a0
giorgioGTelian <giorgiobiologist@gmail.com>
2023-01-29 12:23:25 +0100

    lession_8

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ const app = Vue.createApp ( {
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'useful for keeping your feet warm',
-            image: '/assets/images/socks_green.jpg',
+            selectedVariant: 0,
+            //image: '/assets/images/socks_green.jpg',
             url: '/assets/images/socks_blue.jpg',
-            inStock: true,
+            //inStock: true,
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
               ],
             sizes: ['Small','Medium','Large'],
             
@@ -24,11 +26,23 @@ const app = Vue.createApp ( {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
         },
         removeToCart() {
             this.cart -= 1
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        }
     }
 })
